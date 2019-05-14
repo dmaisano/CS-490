4d95dd694de7dd5ef6c6bda6b09cd62f40a7673d
Domenico Maisano <dev.dmaisano@gmail.com>
2019-05-14 12:26:17 -0400

    added material design lite

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
+import { LoginHandler } from './login/login';
+
+const routes = {
+  '/': LoginHandler,
+};
+
 const AppRoot: HTMLDivElement = document.querySelector('body #root');
 
 (function() {
-  console.log({
-    AppRoot,
-  });
+  const routeHandler = routes[getRoute()];
+  routeHandler(AppRoot);
 })();
+
+function getRoute(): string {
+  return window.location.pathname;
+}
