0becf76204b29ad11da938a35af76342e25bb159
Baudin Marku <bm424@njit.edu>
2019-05-24 14:54:45 -0400

    mid

--- login_curl.php
+++ login_curl.php
@@ -36,5 +36,5 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, true);
 
 curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-$backEndResponse = curl_exec($ch);
+$backendResponse = curl_exec($ch);
 curl_close($ch);
