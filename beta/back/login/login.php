6c0251ba23fec4d38b9a767914ba97103d4dadce
Lawrence DeMaio <ld277@njit.edu>
2019-06-01 15:41:11 -0400

    uh

---
+++ beta/back/login/login.php
@@ -0,0 +1,52 @@
+<?php
+
+include '../config/database.php';
+include '../utils.php';
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$jsonString = file_get_contents('php://input');
+$jsonData = json_decode($jsonString, true);
+
+$user = $jsonData['user'];
+$pass = $jsonData['pass'];
+
+// exit if no user / pass
+if (!isset($user) || !isset($pass)) {
+    exit404('missing field');
+}
+
+$db = new Database();
+$pdo = $db->connect();
+
+try {
+    $sql = "SELECT * FROM users WHERE id=:id";
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->bindParam(':id', $user);
+    $stmt->execute();
+
+    $result = $stmt->fetch();
+
+    if ($result) {
+        $hashedPass = $result['pass'];
+
+        // check if passwords match
+        if (password_verify($pass, $hashedPass)) {
+            $response = array('user' => $user);
+        } else {
+            header('HTTP/1.0 403 Forbidden');
+            $response = array('auth' => false);
+        }
+    } else {
+        header('HTTP/1.0 403 Forbidden');
+        $response = array('auth' => false);
+    }
+} catch (PDOException $error) {
+    header('HTTP/1.1 500 Internal Server Error');
+    $response = array('error' => $error);
+    echo json_encode($response);
+}
+
+echo json_encode($response);
--- login_info.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once('../mysqli_connect.php');
-
-$query
-
- ?>
