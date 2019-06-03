78adfb474b666e665973200f1cbb659a2aa6e0f2
Domenico Maisano <dev.dmaisano@gmail.com>
2019-06-03 18:35:00 -0400

    created user pages

--- public/home/home.js
+++ public/home/home.js
@@ -1,16 +1,20 @@
 import { getUser, navigateUrl } from '../utils.js';
 
+// get the user
+const user = getUser();
+
 /**
  * Student / Instructor Page
  * @param {HTMLDivElement} root
  */
 export function HomeHandler(root) {
-  // get the user
-  const user = getUser();
-
   if (!user) {
     navigateUrl('#/login');
   }
+
+  console.log({
+    user,
+  });
 
   switch (user.type) {
     case 'student':
@@ -29,16 +33,30 @@ export function HomeHandler(root) {
 
 const STUDENT_HOME_PAGE = /*html*/ `
   <div class="home">
+    <div class="title">
+      <h1>Student Home</h1>
+      <h2>User: ${user.id.toUpperCase()}</h2>
+    </div>
+
     <div class="links">
-      <div></div>
+      <a href="/#/student/grades" class="btn btn-primary">View Grades</a>
+      <a href="/#/student/exam" class="btn btn-primary">Take Exam</a>
     </div>
   </div>
 `;
 
 const INSTRUCTOR_HOME_PAGE = /*html*/ `
   <div class="home">
+    <div class="title">
+      <h1>Instructor Home</h1>
+      <h2>User: ${user.id.toUpperCase()}</h2>
+    </div>
+
     <div class="links">
-      <div></div>
+      <a href="/#/instructor/grades" class="btn btn-primary">View Student Grades</a>
+      <a href="/#/instructor/exams" class="btn btn-primary">View Exams</a>
+      <a href="/#/exam/create" class="btn btn-primary">Create Exam</a>
+      <a href="/#/questions/create" class="btn btn-primary">Create Questions</a>
     </div>
   </div>
 `;
