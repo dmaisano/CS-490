1cc42dcf551862906e9df43ccee36b5f1f98431e
Domenico Maisano <dev.dmaisano@gmail.com>
2019-06-06 17:20:03 -0400

    added create user

---
+++ back/login/create-user.php
@@ -0,0 +1,38 @@
+<?php
+
+include '../config/database.php';
+include '../utils.php';
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$jsonString = file_get_contents('php://input');
+$jsonData = json_decode($jsonString, true);
+
+$user = $jsonData['user'];
+$pass = $jsonData['pass'];
+
+// exit if no user / pass
+if (!isset($user) || !isset($pass)) {
+    exit404('missing field');
+}
+
+$hashedPass = password_hash($pass, PASSWORD_DEFAULT);
+
+$db = new Database();
+$pdo = $db->connect();
+
+try {
+    $sql = "INSERT INTO users VALUES (?, ?)";
+    $stmt = $pdo->prepare($sql);
+
+    $args = array($user, $hashedPass);
+    $status = $stmt->execute($args);
+
+    $response = array('success' => true, 'msg' => 'successfully added user "' . $user . '"');
+} catch (PDOException $error) {
+    $response = array('error' => $error);
+    header('HTTP/1.1 500 Internal Server Error');
+}
+
+echo json_encode($response);
