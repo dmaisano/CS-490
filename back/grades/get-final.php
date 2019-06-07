91e718a04d9b1bd30987e7a424d780125b1395e2
Lawrence DeMaio <ld277@github.com>
2019-06-07 10:16:25 -0400

    added get-final (for finalized grade)

--- back/grades/get-final.php
+++ back/grades/get-final.php
@@ -6,13 +6,24 @@ include '../utils.php';
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+$jsonString = file_get_contents('php://input');
+$jsonData = json_decode($jsonString, true);
+
+$user = $jsonData['user'];
+
 $db = new Database();
 $pdo = $db->connect();
 
 try {
     $result = array();
 
-    $stmt = $pdo->query("SELECT * FROM questions");
+    // tldr; i want the grades for a particular student and the grade has been finalized
+    $sql = "SELECT * FROM grades WHERE student_id = ? AND finalized = true";
+    $stmt = $pdo->prepare($sql);
+
+    $args = array($user);
+
+    $status = $stmt->execute($args);
 
     while ($row = $stmt->fetch()) {
         array_push($result, $row);
