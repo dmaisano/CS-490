215be4977e20324d8c947eee5781daaeec035e9c
Lawrence DeMaio <ld277@github.com>
2019-06-07 10:30:13 -0400

    changed JSON to LONGTEXT in sql files

--- sql/exams.sql
+++ sql/exams.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS exams;
 CREATE TABLE IF NOT EXISTS exams (
   id INT AUTO_INCREMENT,
   exam_name VARCHAR(255) NOT NULL, -- Midterm I
-  questions JSON NOT NULL, -- array of question objects
-  points JSON NOT NULL, -- [50, 50]
+  questions LONGTEXT NOT NULL, -- array of question objects
+  points LONGTEXT NOT NULL, -- [50, 50]
 
   PRIMARY KEY(id)
 );
--- sql/grades.sql
+++ sql/grades.sql
@@ -3,12 +3,12 @@ DROP TABLE IF EXISTS grades;
 
 CREATE TABLE IF NOT EXISTS grades (
   id INT AUTO_INCREMENT,
-  exam JSON, -- exam object
+  exam LONGTEXT, -- exam object
   student_id VARCHAR(255),
-  responses JSON,
-  instructor_comments JSON,
-  points JSON, -- [50, 50]
-  points_earned JSON, -- [25, 50]
+  responses LONGTEXT,
+  instructor_comments LONGTEXT,
+  points LONGTEXT, -- [50, 50]
+  points_earned LONGTEXT, -- [25, 50]
   finalized BOOLEAN,
 
   PRIMARY KEY(id),
