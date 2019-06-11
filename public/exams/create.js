d5ecc23cd47dac8a21105580b375e04b561f6893
Domenico Maisano <dev.dmaisano@gmail.com>
2019-06-10 21:32:43 -0400

    working on view exam

--- public/exams/create.js
+++ public/exams/create.js
@@ -4,7 +4,6 @@ import {
   renderBank,
   renderTopics,
 } from '../utils.js';
-import { CREATE_EXAM_PAGE } from './create.page.js';
 
 /** @type {Question[]} */
 let questions;
@@ -39,6 +38,38 @@ export function CreateExamHandler(root) {
   });
 
   renderTopics();
+}
+
+/**
+ * @returns {string}
+ */
+export function CREATE_EXAM_PAGE() {
+  return /*html*/ `
+  <div class="split">
+    <div id="question-bank"></div>
+
+    <div class="new-exam">
+      <h1 class="title">Create Exam</h1>
+
+      <div class="form-group exam-name">
+        <label>Exam Name</label>
+        <input type="text" id="exam-name" placeholder="Enter exam name" required />
+      </div>
+
+      <h2 class="title">Exam Questions</h2>
+
+      <button id="create-exam-btn" class="btn btn-success">Create Exam</button>
+
+      <div id="exam-box">
+        <div id="placeholder">
+          <input type="text" value="No Questions" disabled/>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+`;
 }
 
 /**
