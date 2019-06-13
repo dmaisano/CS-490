97fe30e89208d94c52ee7ade306d733569005500
Domenico Maisano <dev.dmaisano@gmail.com>
2019-06-13 10:02:44 -0400

    update owo

--- public/urls.js
+++ public/urls.js
@@ -2,7 +2,7 @@ export const isDev = false;
 
 const baseUrl = isDev
   ? `http://localhost:5000` // localhost php
-  : `https://web.njit.edu/~ld277/490/back`; // lawrence
+  : `https://web.njit.edu/~dm583/490/back`; // lawrence
 
 export const URLS = {
   login: `${baseUrl}/login/login.php`,
