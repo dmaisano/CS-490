d17d146326460498effbbfd30633a30c2c4ba1f5
Domenico Maisano <dev.dmaisano@gmail.com>
2019-06-13 00:35:40 -0400

    changed users table

--- sql/users.sql
+++ sql/users.sql
@@ -1,14 +1,10 @@
 -- users.sql
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS demo_users;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS demo_users (
   id VARCHAR(255),
   pass VARCHAR(255) NOT NULL,
-  type ENUM('student', 'instructor') NOT NULL,
+  type VARCHAR(255) NOT NULL,
 
   PRIMARY KEY(id)
 );
-
--- Sample Users
--- INSERT INTO users VALUES ('dm583', 'foo', 'student');
--- INSERT INTO users VALUES ('itani', 'foo', 'instructor');
